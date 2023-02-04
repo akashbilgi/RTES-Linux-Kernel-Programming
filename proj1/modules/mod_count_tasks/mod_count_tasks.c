ab5cd41022aac2fc95d0e711fb33e4018df3f083
ubuntu <ubuntu@ubuntu-vm.myguest.virtualbox.org>
2023-02-03 22:38:16 -0800

    little more under standable: trying to break into

--- proj1/modules/mod_count_tasks/mod_count_tasks.c
+++ proj1/modules/mod_count_tasks/mod_count_tasks.c
@@ -10,29 +10,8 @@
 
 #define __NR_count_rt_tasks_mod 449
 
-//extern void * sys_call_table[];
-//EXPORT_SYMBOL(sys_call_table);
-unsigned long **sys_call_table;
-
-static unsigned long **find_sys_call_table(void) {
-    unsigned long **sctable;
-    unsigned long ptr;
-
-    sctable = NULL;
-    for ((unsigned long)sys_call_table;
-         ptr < 0xfffffffffffff000UL; ptr += sizeof(void *)) {
-        unsigned long *p;
-
-        p = (unsigned long *)ptr;
-        if (p[__NR_close] == (unsigned long)sys_call_table[__NR_close]) {
-            sctable = (unsigned long **)p;
-            return sctable;
-        }
-    }
-
-    return NULL;
-}
-
+unsigned long *sys_call_table;
+EXPORT_SYMBOL(sys_call_table);
 
 asmlinkage long count_rt_tasks_mod(int *result)
 {
@@ -50,21 +29,14 @@ asmlinkage long (*orig_count_rt_tasks)(int *result);
 
 static int __init mod_count_tasks_init(void)
 {
-
-    if (!(sys_call_table = find_sys_call_table()))
-        return -1;
-    write_cr0(read_cr0() & (~0x10000));
     orig_count_rt_tasks = (void *)sys_call_table[__NR_count_rt_tasks_mod];
     sys_call_table[__NR_count_rt_tasks_mod] = (unsigned long)count_rt_tasks_mod;
-    write_cr0(read_cr0() | 0x10000);
     return 0;
 }
 
 static void __exit mod_count_tasks_exit(void)
 {
-    write_cr0(read_cr0() & (~0x10000));
     sys_call_table[__NR_count_rt_tasks_mod] = (unsigned long)orig_count_rt_tasks;
-    write_cr0(read_cr0() | 0x10000);
 }
 MODULE_LICENSE("GPL");
 module_init(mod_count_tasks_init);
